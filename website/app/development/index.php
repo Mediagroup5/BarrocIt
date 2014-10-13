1639f8b329b8630e0e3f46a8a9f2983186c35a9f
jordynegen11 <jordy.visser@hotmail.com>
2014-10-13 16:25:39 +0200

    header fix 6

--- website/app/development/index.php
+++ website/app/development/index.php
@@ -5,10 +5,7 @@ include '../../config/config.php';
 include $rootlink. '/config/function.security.php';
 require $rootlink. '/app/templates/header.php';
 ?>
-<link rel="stylesheet" href="development.css"/>
-<link rel="stylesheet" type="text/css" href="http://bootswatch.com/paper/bootstrap.min.css"/>
-<div class="container">
-<div class="container">
+
     <div class="banner">
         <h1 class="bannertxt">Development</h1>
     </div>
