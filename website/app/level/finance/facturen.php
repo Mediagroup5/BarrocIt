11e1bf0477a75fb1acf67689d7e69af979c806a2
Rick <RickVerheijen1@hotmail.com>
2014-10-14 16:09:28 +0200

    finance

--- website/app/level/finance/facturen.php
+++ website/app/level/finance/facturen.php
@@ -6,9 +6,6 @@ include $rootlink. '/config/function.security.php';
 require $rootlink. '/app/templates/header.php';
 ?>
 <div class="container">
-    <div class="banner">
-        <h1 class="bannertxt">Finance</h1>
-    </div>
     <h2 class="ha2">Facturen</h2>
     <table class="table table-striped">
         <thead>
