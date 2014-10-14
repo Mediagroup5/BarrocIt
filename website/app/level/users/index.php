5abf76081464713b9498545a6ce84131105d33f5
jordynegen11 <jordy.visser@hotmail.com>
2014-10-14 15:35:43 +0200

    portfolio update

--- website/app/level/users/index.php
+++ website/app/level/users/index.php
@@ -6,7 +6,7 @@ require $rootlink. '/app/templates/header.php';
   
 		
   
-    <h2 class="ha2">Klanten</h2>
+    <h2 class="ha2">Portfolio</h2>
     <table class="table table-striped">
         <thead>
         <tr>
@@ -20,25 +20,17 @@ require $rootlink. '/app/templates/header.php';
         </thead>
         <tbody>
         <?php
-       $query = $con->query("SELECT * FROM portflio");
+       $query = $con->query("SELECT * FROM portfolio");
         if (mysqli_num_rows($query) > 0 ){
             while ($row = mysqli_fetch_object($query)){
                 echo "<tr>";
-                echo "<td>" . $row->klant_nr . "</td>";
-                echo "<td>" . $row->bedrijfs_naam . "</td>";
-                echo "<td>" . $row->bankrekeningnummer . "</td>";
-                echo "<td>" . $row->crediet . "</td>";
-                echo "<td>" . $row->inkomsten . "</td>";
-                echo "<td>" . $row->limiet . "</td>";
-                echo "<td>" . $row->grootboekrekeningnummer . "</td>";
-                echo "<td>" . $row->bkr . "</td>";
-                echo "<td>" . $row->activated_facturen . "</td>";
-                echo "<td>" . $row->deactivated_facturen . "</td>";
-                echo "<td>" . $row->voorletter . "</td>";
-                echo "<td>" . $row->voornaam . "</td>";
-                echo "<td>" . $row->achternaam . "</td>";
-                echo "<td><a href='facturen.php?id=". $row->klant_nr . "'> Factuur bekijk </a></td>";
-                echo "<td><a href='klant.php?id=" . $row->klant_nr . "'> Klant gegevens </a></td>";
+                echo "<td>----</td>";
+                echo "<td>" . $row->type . "</td>";
+                echo "<td>" . $row->omschrijving . "</td>";
+                echo "<td>" . $row->aanv_datum . "</td>";
+                echo "<td>" . $row->eind_datum . "</td>";
+                echo "<td>" . $row->opmerking . "</td>";
+                echo "<td><a href='edit.php?id=". $row->port_id . "'> portfolio bewerken </a></td>";
                 echo "</tr>";
             }
         }
