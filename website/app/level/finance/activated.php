f2f7024cfd1eff07087fdc39b555a134a9d44b62
MW155836 <mitchwalravens2@gmail.com>
2014-10-28 16:24:56 +0100

    nieuw

---
+++ website/app/level/finance/activated.php
@@ -0,0 +1,65 @@
+<?php  
+$page = "finance";
+$id = "Activated";
+include '../../../config/config.php';
+include $rootlink. '/config/function.security.php';
+require $rootlink. '/app/templates/header.php';
+?>
+
+   <div class="container">
+    <h2 class="ha2">Activated</h2>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Klant nummer</th>
+            <th>Factuur nummer</th>
+            <th>Bedrag</th>
+            <th>Project nummer</th>
+            <th>BTW</th>
+            <th>Begin Datum</th>
+            <th>Verval Datum</th>
+            <th>Offer status</th>
+            <th>Hoeveelheid</th>
+            <th>Beschrijving</th>
+            <th>Aantal</th>
+            <th>Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+
+        $sql = "SELECT * FROM factuur WHERE klant_nr = '".Security($_GET['id'])."'";
+        if (! $query = mysqli_query($con, $sql)){
+            echo "Kan gegevens niet uit database halen";
+        }
+        $sql = "SELECT factuur_tot FROM factuur WHERE DATE > factuur_tot";
+        if (mysqli_num_rows($query) > 0 ){
+            while ($row = mysqli_fetch_object($query)){
+                echo "<tr>";
+                echo "<td>" . $row->klant_nr . "</td>";
+                echo "<td>" . $row->factuur_nr . "</td>";
+                echo "<td>" . $row->bedrag . "</td>";
+                echo "<td>" . $row->project_nr . "</td>";
+                echo "<td>" . $row->btw . "</td>";
+                echo "<td>" . $row->factuur_tot . "</td>";
+                echo "<td>" . $row->factuur_begin . "</td>";
+                echo "<td>" . $row->hoeveelheid . "</td>";
+                echo "<td>" . $row->beschrijving . "</td>";
+                echo "<td>" . $row->aantal . "</td>";
+                echo "<td>" . $row->status . "</td>";
+                echo "<td><a href='edit.php?id=". $row->klant_nr . "'> Bewerk </a></td>";
+                echo "<td><a href='delete.php?id=" . $row->klant_nr . "'> X </a></td>";
+                echo "</tr>";
+            }
+        }
+
+
+
+        ?>
+        </tbody>
+    </table>
+  
+   <?php
+include $rootlink. "/app/templates/footer.php";
+?>
+</div>
