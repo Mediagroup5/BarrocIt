86240072cacb1bcfac2a53d72a7df51d1c93cb5d
jordynegen11 <jordy.visser@hotmail.com>
2014-10-28 16:51:40 +0100

    vnv/donderdag mee verder gaan

--- website/app/level/finance/add.php
+++ website/app/level/finance/add.php
@@ -4,6 +4,41 @@ $id = "add";
 include '../../../config/config.php';
 include $rootlink. '/config/function.security.php';
 require $rootlink. '/app/templates/header.php';
+
+if(isset($_POST['submit'])){
+
+//$a = strtotime($_POST['factuur_duur'], '%Y-%m-%d');
+//$timestamp = mktime(0, 0, 0, $a['tm_mon']+1, $a['tm_mday'], $a['tm_year']+1900);
+
+//die($timestamp);
+
+
+    if(! empty($_POST['klant_nr']) && ! empty($_POST['bedrag']) && ! empty($_POST['project_nr']) && ! empty($_POST['btw'])
+    && ! empty($_POST['factuur_duur']) && ! empty($_POST['hoeveelheid']) && ! empty($_POST['beschrijving'])
+        && ! empty($_POST['aantal']) && ! empty($_POST['status'])){
+        $klant_nr = mysqli_real_escape_string($con, $_POST['klant_nr']);
+        $bedrag = mysqli_real_escape_string($con, $_POST['bedrag']);
+        $project_nr = mysqli_real_escape_string($con, $_POST['project_nr']);
+        $btw = mysqli_real_escape_string($con, $_POST['btw']);
+        $factuur_duur = mysqli_real_escape_string($con, $_POST['factuur_duur']);
+        $hoeveelheid = mysqli_real_escape_string($con, $_POST['hoeveelheid']);
+        $beschrijving = mysqli_real_escape_string($con, $_POST['beschrijving']);
+        $aantal = mysqli_real_escape_string($con, $_POST['aantal']);
+        $status = mysqli_real_escape_string($con, $_POST['status']);
+
+        $sql = "INSERT INTO factuur (klant_nr, bedrag, project_nr, btw, factuur_duur, hoeveelheid,
+beschrijving, aantal, status) VALUES ('$klant_nr', '$bedrag', '$project_nr', '$btw',
+ '$factuur_duur', '$hoeveelheid', '$beschrijving', '$aantal', '$status' )";
+
+        if (! $query = mysqli_query($con, $sql)){
+            echo 'Factuur toevoegen is niet gelukt...</a>';
+        }else{
+            header('location: index.php');
+        }
+    }else{
+        header('location: index.php');
+    }
+}
 ?>
 
     <form action="add.php" method="post" class="form col-md-12">
@@ -50,32 +85,5 @@ require $rootlink. '/app/templates/header.php';
 
     </form>
 <?php
-if(isset($_POST['submit'])){
 
-    if(! empty($_POST['klant_nr']) && ! empty($_POST['bedrag']) && ! empty($_POST['project_nr']) && ! empty($_POST['btw'])
-    && ! empty($_POST['factuur_duur']) && ! empty($_POST['hoeveelheid']) && ! empty($_POST['beschrijving'])
-        && ! empty($_POST['aantal']) && ! empty($_POST['status'])){
-        $klant_nr = mysqli_real_escape_string($con, $_POST['klant_nr']);
-        $bedrag = mysqli_real_escape_string($con, $_POST['bedrag']);
-        $project_nr = mysqli_real_escape_string($con, $_POST['project_nr']);
-        $btw = mysqli_real_escape_string($con, $_POST['btw']);
-        $factuur_duur = mysqli_real_escape_string($con, $_POST['factuur_tot']);
-        $hoeveelheid = mysqli_real_escape_string($con, $_POST['hoeveelheid']);
-        $beschrijving = mysqli_real_escape_string($con, $_POST['beschrijving']);
-        $aantal = mysqli_real_escape_string($con, $_POST['aantal']);
-        $status = mysqli_real_escape_string($con, $_POST['status']);
-
-        $sql = "INSERT INTO factuur (klant_nr, bedrag, project_nr, btw, factuur_duur, hoeveelheid,
-beschrijving, aantal, status) VALUES ('$klant_nr', '$bedrag', '$project_nr', '$btw',
- '$factuur_duur', '$hoeveelheid', '$beschrijving', '$aantal', '$status' )";
-
-        if (! $query = mysqli_query($con, $sql)){
-            echo 'Factuur toevoegen is niet gelukt...</a>';
-        }else{
-            header('location: index.php');
-        }
-    }else{
-        header('location: index.php');
-    }
-}
 include $rootlink. "/app/templates/footer.php";
