9aae4c7282caa34909e8e75a5fcc4230ca337874
jordynegen11 <jordy.visser@hotmail.com>
2014-10-31 11:56:00 +0100

    Afspraken auto klant id

--- website/app/level/sales/addafspraak.php
+++ website/app/level/sales/addafspraak.php
@@ -22,11 +22,12 @@ require $rootlink. '/app/templates/header.php';
 		$datum 			= 		Security($_POST['datum']); //variabele aanmaken
 		$naam 		= 	Security($_POST['naam']);//variabele aanmaken
 		$tijd 			= 		Security($_POST['tijd']);//variabele aanmaken
+		$klant = Security($_POST['klant']);
 		$plaats 	= Security($_POST['plaats']);//variabele aanmaken
 		$opmerkingen 	= Security($_POST['opmerkingen']);//variabele aanmaken
 
-		if (!$query 	= mysqli_query($con,"INSERT INTO afspraken (datum, naam, tijd, plaats, opmerkingen) 
-											VALUES ('$datum', '$naam','$tijd', '$plaats', '$opmerkingen')"))  //hier voegt hij toe waar het precies inmoet in database.
+		if (!$query 	= mysqli_query($con,"INSERT INTO afspraken (datum, klant_nr, naam, tijd, plaats, opmerkingen) 
+											VALUES ('$datum', '".$klant."', '$naam','$tijd', '$plaats', '$opmerkingen')"))  //hier voegt hij toe waar het precies inmoet in database.
 		{
 			echo 'kan data niet toevoegen aan database'; //alshet niet lukt krijg je dit 
 		}else{
@@ -54,26 +55,36 @@ require $rootlink. '/app/templates/header.php';
 			<form action="" method="POST">
 				<label for="datum">datum</label>
 				<input type="date" name="datum" id="datum" required>
-
+                <br><br>
 				
 
 				<label for="naam">naam</label>
 				<input type="text" name="naam" id="naam" required>
-
-
+				<br><br>
+				
+<label for="klant">Klant</label>
+<select name="klant">
+<?php
+$sql = $con->query("SELECT klant_nr,bedrijfs_naam FROM klantgegevens");
+while($row = mysqli_fetch_object($sql))
+				
+  echo'<option value="'.$row->klant_nr.'">'.$row->bedrijfs_naam.'</option>';
+  ?>
+</select>
+<br><br>
 				<label for="tijd">tijd</label>
 				<input type="time" name="tijd" id="tijd" required>
 
 				
-				
+				<br><br>
 
 				<label for="plaats">plaats</label>
 				<input type="text" name="plaats" id="plaats" required>
-
+                <br><br>
 
 				<label for="opmerkingen">opmerkingen</label>
 				<textarea name="opmerkingen" id="opmerkingen" cols="30" required></textarea>
-				
+				<br><br>
 				<input name="submit" type="submit" value="toevoegen">
 		
 				</form>
