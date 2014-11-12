564516253b921fc44e942944091f0f543f47c304
jordynegen11 <jordy.visser@hotmail.com>
2014-11-12 16:24:15 +0100

    db fix 2

--- website/config/config.php
+++ website/config/config.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 
 //MYSQLI class
 
-$dbconfig = mysqli_connect("127.0.0.1","root","","barroc-it");
+$dbconfig = mysqli_connect("127.0.0.1","root","","barroc_it");
 //Wesite config ophalen
 $sqlconf = $dbconfig->query("SELECT * FROM site_config");
 $conf = mysqli_fetch_object($sqlconf);
@@ -22,7 +22,7 @@ $dbuser = "root";
 //Database wachtwoord
 $dbpass = "";
 //Database naam
-$dbname = "barroc-it";
+$dbname = "barroc_it";
 
 $con = new DB($host,$dbuser,$dbpass,$dbname);
     
